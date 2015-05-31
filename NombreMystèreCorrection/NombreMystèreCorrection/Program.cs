299d7afd45094f7e13a35914212685e2e6bafe91
Simon Cambier <belgegwada@outlook.com>
2015-05-31 17:27:03 +0200

    Correction finale

--- NombreMystèreCorrection/NombreMystèreCorrection/Program.cs
+++ NombreMystèreCorrection/NombreMystèreCorrection/Program.cs
@@ -10,41 +10,30 @@ namespace NombreMystèreCorrection
     {
         static void Main(string[] args)
         {
-
             int randome = new Random().Next(1, 101);
-
             // l'utilisateur choisis un nombre
             Console.WriteLine("saississez un nombre entre 1 et 100");
             string nombrealeatoire = Console.ReadLine();
-            // verifier la validité de la saisie avec Tryparse.
             int nombrealeatoireconverti = Convert.ToInt32(nombrealeatoire);
-
-            while (true)
+            while (randome != nombrealeatoireconverti)
             {
-
-
                 if (nombrealeatoireconverti < randome)
                 {
                     Console.WriteLine("votre nombre est trop petit");
+                    nombrealeatoire = Console.ReadLine();
+                    nombrealeatoireconverti = Convert.ToInt32(nombrealeatoire);
                 }
-
-
                 else if (nombrealeatoireconverti > randome)
                 {
                     Console.WriteLine("votre nombre est trop grand ");
-
+                    nombrealeatoire = Console.ReadLine();
+                    nombrealeatoireconverti = Convert.ToInt32(nombrealeatoire);
                 }
-
-
-                else if (nombrealeatoireconverti == randome)
-                {
-                    Console.WriteLine("Bravo vous avec trouver le nombre");
-
-                    // j'avais fait le programme de ce style ci en pythonConsole.WriteLine("Bravo vous avec trouver le nombre");
-
-                    // j'avais fait le programme de ce style ci en python
-                }
-
+            }
+            if (randome == nombrealeatoireconverti)
+            {                
+                Console.WriteLine("Bravo vous avec trouver le nombre");
+                Console.ReadKey();
             }
 
         }
